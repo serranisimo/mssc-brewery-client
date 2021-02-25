77a8be0e7b6f3307667e1b4ed9261a919a5adfbf
serranisimo <9xdbdRqzPskzf6x9TNsH>
2021-02-25 16:31:48 +0100

    Brewery Client post, update delete

--- src/main/java/com/serrano/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/serrano/msscbreweryclient/web/client/BreweryClient.java
@@ -2,12 +2,11 @@ package com.serrano.msscbreweryclient.web.client;
 
 import com.serrano.msscbreweryclient.config.BreweryClientProperties;
 import com.serrano.msscbreweryclient.web.model.BeerDto;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
@@ -26,5 +25,22 @@ public class BreweryClient {
         var apiHost = clientProperties.getApiHost();
         var beerResourcePath = clientProperties.getBeerResourcePath();
         return this.restTemplate.getForObject(apiHost + beerResourcePath + "/" + beerId.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto){
+        return this.restTemplate.postForLocation(
+                this.clientProperties.getApiHost() + this.clientProperties.getBeerResourcePath(),
+                beerDto);
+    }
+
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        restTemplate.put(
+                clientProperties.getApiHost() + clientProperties.getBeerResourcePath() + "/" + beerId,
+                beerDto
+        );
+    }
+
+    public void deleteBeer(UUID beerId){
+        this.restTemplate.delete(this.clientProperties.getApiHost() + this.clientProperties.getBeerResourcePath() + "/" + beerId);
     }
 }
--- src/test/java/com/serrano/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/serrano/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,10 +1,12 @@
 package com.serrano.msscbreweryclient.web.client;
 
 import com.serrano.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +22,30 @@ class BreweryClientTest {
         BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
         System.out.println(beerDto.toString());
         assertNotNull(beerDto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beer = BeerDto.builder().beerName("Test Beer").beerStyle("Pale ale").build();
+
+        URI uri = breweryClient.saveNewBeer(beer);
+
+        assertNotNull(uri);
+        System.out.println(uri);
+    }
+
+    @Test
+    void updateBeer() {
+        BeerDto beerDto = BeerDto.builder().id(UUID.randomUUID()).beerName("Oleeee Servesita").build();
+
+        Assertions.assertDoesNotThrow(() -> this.breweryClient.updateBeer(beerDto.getId(), beerDto));
+    }
+
+    @Test
+    void deleteBeer() {
+        UUID beerId = UUID.randomUUID();
+        Assertions.assertDoesNotThrow(() -> {
+            this.breweryClient.deleteBeer(beerId);
+        });
     }
 }
